8ec5c029cf26a681f4417c74f7453a68207ebfad
Javi Miguel <jamg44@gmail.com>
2017-09-19 15:37:06 +0200

    ejemplos de inyección de dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { EjemplosComponentesComponent } from "./ejemplos/ejemplos-componentes.co
 import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 import { ContactosService } from "./servicios/contactos.service";
 import { DetallesContactoComponent } from './detalles-contacto/detalles-contacto.component';
+import { Servicio1,
+  EjemplosInyeccionComponent,
+  Servicio2Provider,
+  Servicio3Provider,
+  MiValorProvider} from "./ejemplos/ejemplos-inyeccion.component";
 
 @NgModule({
   declarations: [ // apuntamos todos los componentes, pipas y directivas
@@ -17,14 +22,19 @@ import { DetallesContactoComponent } from './detalles-contacto/detalles-contacto
     EjemplosBindingComponent,
     EjemplosComponentesComponent,
     ListaContactosComponent,
-    DetallesContactoComponent
+    DetallesContactoComponent,
+    EjemplosInyeccionComponent
   ],
   imports: [ // apuntamos modulos externos que uso
     BrowserModule,
     FormsModule // para usar formularios
   ],
   providers: [ // apuntamos dependencias para inyectar
-    ContactosService
+    ContactosService,
+    Servicio1, // decorado con injectable
+    Servicio2Provider, // sin decorador, creado con proveedor de clase
+    Servicio3Provider, // sin decorador, creado con proveedor de factoria
+    MiValorProvider // proveedor de valor
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/ejemplos/ejemplos-inyeccion.component.ts
@@ -0,0 +1,72 @@
+import { Component, Injectable, OnInit, OpaqueToken, Inject } from '@angular/core';
+
+@Injectable()
+export class Servicio1 {
+  obtenerMensaje() {
+    return 'Soy un servicio decorado con @Injectable';
+  }
+}
+
+// Como no usamos @Injectable, le creamos un proveedor de clase
+export class Servicio2 {
+  obtenerMensaje() {
+    return 'Soy un servicio que usa un proveedor de clase';
+  }
+}
+export const Servicio2Provider = {
+  provide: Servicio2,
+  useClass: Servicio2
+};
+
+//proveedor de factoría
+export class Servicio3 {
+  numeroDeClientes: number;
+  constructor(private mensaje: string) { }
+  obtenerMensaje() {
+    return this.mensaje;
+  }
+}
+export const Servicio3Provider = {
+  provide: Servicio3,
+  useFactory: () => {
+    // construimos el resultado
+    return new Servicio3('Soy un servicio que usa un proveedor de factoria');
+    // return { obtenerMensaje: () => 'aaa' };
+  }
+};
+
+// Ejemplo de proveedor de valor
+export const MiValor: OpaqueToken = new OpaqueToken('MiValor');
+export const MiValorProvider = {
+  provide: MiValor,
+  useValue: 'Soy un valor inyectado como dependencia'
+};
+
+// Componente que usa los servicios de arriba
+@Component({
+  selector: 'ejemplos-inyeccion',
+  templateUrl: 'ejemplos-inyeccion.component.html'
+})
+
+export class EjemplosInyeccionComponent implements OnInit {
+
+  mensaje1: string;
+  mensaje2: string;
+  mensaje3: string;
+  mensaje4: string;
+
+  constructor(
+    private servicio1: Servicio1,
+    private servicio2: Servicio2,
+    private servicio3: Servicio3,
+    @Inject(MiValor) private valor: string
+  ) { }
+
+  ngOnInit() {
+    this.mensaje1 = this.servicio1.obtenerMensaje();
+    this.mensaje2 = this.servicio2.obtenerMensaje();
+    this.mensaje3 = this.servicio3.obtenerMensaje();
+    this.mensaje4 = this.valor;
+  }
+
+}
